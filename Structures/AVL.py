d9e81651c038830e13faed78a46eb200d3ff7715
Luis Rivera <53579929+LuisRivera96@users.noreply.github.com>
2019-10-16 00:28:31 -0600

    1.3

--- Structures/AVL.py
+++ Structures/AVL.py
@@ -1,2 +1,244 @@
+import os
+import subprocess
 class nodeAVL:
+    def __init__(self,CARNE,NAME):
+        self.CARNE = CARNE
+        self.NAME = NAME
+        self.ALTURA = 1
+        self.FE = 0
+        self.RIGHT = None
+        self.LEFT = None
+
+class AVL:
+    def __init__(self):
+        self.root = None
+
+    def add(self,CARNET,NAME):
+        self.root = self.addRecursived(self.root,CARNET,NAME)
+
+    def addRecursived(self,ROOT,CARNET,NAME):
+        if ROOT is None:
+            nuevo = nodeAVL(CARNET,NAME)
+            return nuevo
+        if CARNET < ROOT.CARNE:
+            ROOT.LEFT = self.addRecursived(ROOT.LEFT,CARNET,NAME)
+        if CARNET > ROOT.CARNE:
+            ROOT.RIGHT = self.addRecursived(ROOT.RIGHT,CARNET,NAME)      
+
+        ROOT.ALTURA = 1 + max(self.getHeight(ROOT.LEFT),self.getHeight(ROOT.RIGHT))
+        balance = self.getBalance(ROOT)
+
+        if balance > 1 and CARNET < ROOT.LEFT.CARNE:
+            return self.rightRotate(ROOT)
+
+        if balance < -1 and CARNET > ROOT.RIGHT.CARNE:
+            return self.leftRotate(ROOT)
+
+        if balance > 1 and CARNET > ROOT.LEFT.CARNE:
+            ROOT.LEFT = self.leftRotate(ROOT.LEFT)
+            return self.rightRotate(ROOT)
+
+        if balance < -1 and CARNET < ROOT.RIGHT.CARNE:
+            ROOT.RIGHT = self.rightRotate(ROOT.RIGHT)
+            return self.leftRotate(ROOT)
+
+        return ROOT
+            
+                   
+
+
     
+    def leftRotate(self,z):
+        y = z.RIGHT
+        T2 = y.LEFT
+        y.LEFT = z
+        z.RIGHT = T2
+        z.ALTURA = 1 + max(self.getHeight(z.LEFT),self.getHeight(z.RIGHT))
+        y.ALTURA = 1 + max(self.getHeight(y.LEFT),self.getHeight(y.RIGHT))
+        return y
+
+    
+    def rightRotate(self,z):
+        y = z.LEFT
+        T3 = y.RIGHT
+        y.RIGHT = z
+        z.LEFT = T3
+        z.ALTURA = 1 + max(self.getHeight(z.LEFT),self.getHeight(z.RIGHT))
+        y.ALTURA = 1 + max(self.getHeight(y.LEFT),self.getHeight(y.RIGHT))
+        return y
+
+    def getHeight(self,RAIZ):
+        if RAIZ is None:
+            return 0
+        return RAIZ.ALTURA
+
+    def getBalance(self,RAIZ):
+        if RAIZ is None:
+            return 0
+        return self.getHeight(RAIZ.LEFT) - self.getHeight(RAIZ.RIGHT)                
+
+    #BUSQUEDA
+    def Busqueda(self,CARNE):
+        return self.BusquedaR(self.root,CARNE)
+    def BusquedaR(self,ROOT,CARNE):
+        if ROOT is None:
+            return None
+        elif CARNE == ROOT.CARNE:
+            return ROOT
+        elif CARNE < ROOT.CARNE:
+            return self.BusquedaR(ROOT.LEFT,CARNE)
+        elif CARNE > ROOT.CARNE:
+            return self.BusquedaR(ROOT.RIGHT,CARNE)
+
+
+    def balanced(self,ROOT):
+        if ROOT is not None:
+            self.balanced(ROOT.LEFT)
+            if self.getBalance(ROOT) > 1 or self.getBalance(ROOT) < -1:
+                return FalseS
+            self.balanced(ROOT.RIGHT)        
+
+    #RECORRIDOS
+    #########################INORDEN#########################
+    inor = "INICIO-> "
+    def inorden(self):
+        self.inordenR(self.root)
+    def inordenR(self,ROOT):
+        if ROOT is not None:
+            self.inordenR(ROOT.LEFT)
+            inor = inor + ROOT.CARNE + "-" + ROOT.NAME + "-> "
+            self.inordenR(ROOT.RIGHT)
+        inor = inor + " ->FIN"    
+        print(inor)
+
+    def gInorden(self,ROOT):
+        DotInor = ""
+        if ROOT is not None:
+            DotInor = DotInor + self.gInorden(ROOT.LEFT)
+            DotInor = DotInor + ROOT.CARNE + "[label=\""+ ROOT.CARNE +" "+ROOT.NAME+"\"];\n"
+            DotInor = DotInor + self.gInorden(ROOT.RIGHT)
+        return DotInor
+
+    def getGraphIno(self):
+        Dot = ""
+        Dot = Dot + "digraph BLOCK{\n"
+        Dot = Dot + "label=\"Inorden\";\n"
+        Dot = Dot + "rankdir=\"LR\";\n"
+        Dot = Dot + self.gInorden(self.root)
+        Dot = Dot + "}"
+        f = open('INORDEN.dot','w')
+        f.write(Dot)
+        f.close()    
+        os.system('dot INORDEN.dot -Tpng -o INORDEN.png')
+        os.system('INORDEN.png')
+
+    ##############################################################
+    #########################PREORDEN#########################
+    pre = "INICIO-> "
+    def preorden(self):
+        self.preordenR(self.root)
+    def preordenR(self,ROOT):
+        if ROOT is not None:
+            pre = pre + ROOT.CARNE + "-" + ROOT.NAME + "-> "
+            self.preordenR(ROOT.LEFT)
+            self.preordenR(ROOT.RIGHT)
+        pre = pre + " ->FIN"    
+        print(pre)
+
+    def gPreorden(self,ROOT):
+        DotPreor = ""
+        if ROOT is not None:
+            DotPreor = DotPreor + ROOT.CARNE + "[label=\""+ ROOT.CARNE +" "+ROOT.NAME+"\"];\n"
+            DotPreor = DotPreor + self.gPreorden(ROOT.LEFT)
+            DotPreor = DotPreor + self.gPreorden(ROOT.RIGHT)
+        return DotPreor
+
+    def getGraphPreo(self):
+        Dot = ""
+        Dot = Dot + "digraph BLOCK{\n"
+        Dot = Dot + "label=\"Preorden\";\n"
+        Dot = Dot + "rankdir=\"LR\";\n"
+        Dot = Dot + self.gPreorden(self.root)
+        Dot = Dot + "}"
+        f = open('PREORDEN.dot','w')
+        f.write(Dot)
+        f.close()    
+        os.system('dot PREORDEN.dot -Tpng -o PREORDEN.png')
+        os.system('PREORDEN.png')
+
+    ##############################################################
+    #########################POSTORDEN#########################
+    pos = "INICIO-> "
+    def posorden(self):
+        self.posordenR(self.root)
+    def posordenR(self,ROOT):
+        if ROOT is not None:
+            self.posordenR(ROOT.LEFT)
+            self.posordenR(ROOT.RIGHT)
+            pos = pos + ROOT.CARNE + "-" + ROOT.NAME + "-> "
+        pos = pos + " ->FIN"    
+        print(pos)
+
+    def gPosorden(self,ROOT):
+        DotPosor = ""
+        if ROOT is not None:
+            DotPosor = DotPosor + self.gPosorden(ROOT.LEFT)
+            DotPosor = DotPosor + self.gPosorden(ROOT.RIGHT)
+            DotPosor = DotPosor + ROOT.CARNE + "[label=\""+ ROOT.CARNE +" "+ROOT.NAME+"\"];\n"
+        return DotPosor
+
+    def getGraphPoso(self):
+        Dot = ""
+        Dot = Dot + "digraph BLOCK{\n"
+        Dot = Dot + "label=\"Postorden\";\n"
+        Dot = Dot + "rankdir=\"LR\";\n"
+        Dot = Dot + self.gPosorden(self.root)
+        Dot = Dot + "}"
+        f = open('POSORDEN.dot','w')
+        f.write(Dot)
+        f.close()    
+        os.system('dot POSORDEN.dot -Tpng -o POSORDEN.png')
+        os.system('POSORDEN.png')
+
+    ##############################################################
+    #GRAPHIZAVL
+
+    def graficar(self,ROOT):
+        Dot = ""
+        if ROOT is not None:
+            if ROOT.RIGHT is not None or ROOT.LEFT is not None:
+                Dot = Dot + str(ROOT.CARNE) + ":f" + str(ROOT.CARNE) + "[id=" + str(ROOT.CARNE) + ", color=\"blue\"]; \n"
+                Dot = Dot + str(ROOT.CARNE) + "[label=\" <N " + str(ROOT.CARNE)+ " " + ROOT.NAME + " I> | <f" + str(ROOT.CARNE) + "> " + str(ROOT.CARNE) + "\\n" + " | <f" + str(ROOT.CARNE) + "D> \" shape=\"record\"];\n"
+            else:
+                Dot = Dot + str(ROOT.CARNE) + ":f " + str(ROOT.CARNE) + "[id=" + str(ROOT.CARNE) + ", color=\"blue\" shape=\"rectangle\"]; \n"
+
+            a = self.graficar(ROOT.LEFT)
+            if a is not "":
+                Dot = Dot + "\"" + str(ROOT.CARNE) + "\" : N" + str(ROOT.CARNE) + "I -> "
+                Dot = Dot + " " + a
+            b = self.graficar(ROOT.RIGHT)
+            if b is not "":
+                Dot = Dot + "\"" + str(ROOT.CARNE) + "\" : f" + str(ROOT.CARNE) + "D -> "
+                Dot = Dot + " " + b
+        return Dot    
+
+    def getGrafica(self):
+        dot = ""
+        dot = dot + "digraph AVL{\n"
+        dot = dot + "compound=true;\n"
+        dot = dot + "node[shape=\"Mrecord\"];\n"
+        dot = dot + self.graficar(self.root)
+        dot = dot + "}"
+        f = open('AVL.dot','w')
+        f.write(dot)
+        f.close()    
+        os.system('dot AVL.dot -Tpng -o AVL.png')
+        os.system('AVL.png')        
+
+                
+
+
+
+
+
+
--- Structures/Menu.py
+++ Structures/Menu.py
@@ -1,6 +1,27 @@
 import sys
+import os
+import hashlib
+import csv
+import socket
+import select
+import tkinter as tk
+from datetime import datetime
+from Chain import*
+from AVL import*
+
+
+#var
+arbol = AVL()
+cadena = Chain()
+indexB = 0
+clase = ""
+dataA = ""
+#
+
 class Menu:
+    
     def menuP(self):
+        os.system("cmd /c cls")
         print("############################################################")
         print("###################### MAIN MENU ###########################")
         print("1. Insert Block")
@@ -8,18 +29,156 @@ class Menu:
         print("3. Reports")
         print("4. Salir")
         opcion = input()
-        if opcion  == 1:
-            #crear bloque
-            #verificar
-            #validar
-            #insertar
-        elif opcion == 2:
+        if opcion  == '1':
+            block = self.crearBloque()
+            print(block)
+            self.keypress() 
+        elif opcion == '2':
             print("bloques")
-        elif opcion == 3:
-            #llamar menu reportes
-            print("Reportes")
-        elif opcion == 4:
+        elif opcion == '3':
+            self.menuReportes()
+        elif opcion == '4':
             sys.exit()
         else:
-            self.menuP()      
+            self.menuP()
+
+    def selectBloque(self):
+        os.system("cmd /c cls")
+        print("############################################################")
+        print("###################### SELECT BLOCK ###########################")
+        if self.cadena.head is not None:
+            print('INDEX: '+self.cadena.head.INDEX)
+            print('TIMESTAMP: '+self.cadena.head.TIMESTAMP)
+            print('CLASS: '+self.cadena.head.CLASS)
+            print('DATA: '+self.cadena.head.DATA[0:50])
+            print('PREVIOUSHASH: '+self.cadena.head.PREVIOUSHASH)
+            print('HASH: '+self.cadena.head.HASH)
+
+            
+    def keypress(self,event):
+        if event.keypress == 'KEY_RIGHT':
+            print("derecha")
+        elif event.keypress == 'KEY_LEFT':
+            print("izquierda")
+        elif event.keypress == 'KEY_ENTER':
+            print("enter")        
+            
+
+
+
+    def menuReportes(self):
+        os.system("cmd /c cls")
+        print("############################################################")
+        print("###################### MENU REPORTS ###########################")
+        print("1. BlockChain Report")
+        print("2. Tree Reports")
+        print("3. Regresar")
+        opcion = input()
+        if opcion  == '1':
+            cadena.graphiz()
+        elif opcion == '2':
+            self.reportesTree()
+        elif opcion == '3':
+            self.menuP()
+        else:
+            self.menuReportes()
+
+    def reportesTree(self):
+        print("############################################################")
+        print("###################### TREE REPORTS ###########################")
+        print("1. Visualizar Arbol")
+        print("2. Recorridos")
+        print("3. Regresar")
+        opcion = input()
+        if opcion  == '1':
+            arbol.getGrafica()
+        elif opcion == '2':
+            os.system("cmd /c cls")
+            print("############################################################")
+            print("###################### RECORRIDOS ###########################")
+            print("1. Preorden")
+            print("2. Inorden")
+            print("3. Portorden")
+            print("4. Regresar")
+            opcion2 = input()
+            if opcion2 == '1':
+                arbol.getGraphPreo()
+                arbol.preorden()
+                self.reportesTree()
+            elif opcion2 == '2':
+                arbol.getGraphIno()
+                arbol.inorden()
+                self.reportesTree()
+            elif opcion2 == '3':
+                arbol.getGraphPoso()
+                arbol.posorden()
+                self.reportesTree()
+            elif opcion2 == '4':
+                self.reportesTree()
+            else:
+                self.reportesTree()
+
+        elif opcion == '3':
+            self.menuReportes()
+        else:
+            self.reportesTree()
+
+
+    def bulkL(self):
+        fila = 0
+        os.system("cmd /c cls")
+        print("#######################INSERTAR BLOQUE##########################")
+        print("Ingrese nombre del archivo .csv")
+        nombre = input()
+        nombreB = "bloques/"+nombre
+        datos = []
+        with open(nombreB) as csvfile:
+            readCSV = csv.reader(csvfile,delimiter=',')
+            for row in readCSV:
+                  dato = row[1]
+                  datos.append(dato)
+
+        global clase
+        clase = datos[0]
+        global dataA
+        dataA = datos[1]        
+        print("clase= "+datos[0])
+        print("data= "+datos[1]) 
+        #self.menuP()    
+  
+    def crearBloque(self):
+        self.bulkL()
+        if indexB is 0:
+            now = datetime.now()
+            timeA = now.strftime("%d-%m-%Y::%H:%M:%S")
+            hashN = self.encrypt_string(str(indexB),timeA,clase,dataA,'0000')
+            jsonB = "{\"INDEX\": "+str(indexB)+",\n"+"\"TIMESTAMP\": \""+timeA+"\",\n\"CLASS\": \""+clase+"\",\n\"DATA\": "+dataA+",\n\"PREVIOUSHASH\": \"0000\",\n\"HASH\": \""+hashN+"\"\n}"
+            return jsonB
+        else:
+            now = datetime.now()
+            timeA = now.strftime("%d-%m-%Y::%H:%M:%S")
+            hashN = self.encrypt_string(str(indexB),timeA,clase,dataA,cadena.end.HASH)
+            jsonB = "{\"INDEX\": "+str(indexB)+",\n"+"\"TIMESTAMP\": \""+timeA+"\",\n\"CLASS\": \""+clase+"\",\n\"DATA\": "+dataA+",\n\"PREVIOUSHASH\": \""+cadena.end.HASH+"\",\n\"HASH\": \""+hashN+"\"\n}"
+            return jsonB
+
+    def encrypt_string(self,string,string2,string3,string4,string5):
+        string6 = string+string2+string3+string4,string5
+        sha_encyption = hashlib.sha256(str(string6).encode()).hexdigest()
+        return sha_encyption
+
+    
+
+
+
+    #CLIENTE-SERVIDOR
+
+
+prueba = Menu()
+prueba.menuP()
+
+        
+
+
+
+
         
